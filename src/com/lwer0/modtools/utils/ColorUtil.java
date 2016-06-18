ed183392c87f7084b18b59b0dc5700f5038a77d1
Joel <Joel@192.168.1.177>
2016-06-18 02:12:46 +0200

    creado de 0

---
+++ src/com/lwer0/modtools/utils/ColorUtil.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lwer0.modtools.utils;
+
+import com.lwer0.modtools.ModTools;
+import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
+
+/**
+ *
+ * @author Joel
+ */
+public class ColorUtil implements Listener {
+    
+    public static String color (String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+    public ColorUtil(ModTools aThis) {
+        throw new UnsupportedOperationException("Error.");
+    }
+    
+}
