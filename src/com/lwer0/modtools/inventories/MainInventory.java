65a2852afa71fd7b814f5f70b23a899404c21597
Joel <Joel@192.168.1.177>
2016-06-18 22:33:28 +0200

    inventory click on main inv

--- src/com/lwer0/modtools/inventories/MainInventory.java
+++ src/com/lwer0/modtools/inventories/MainInventory.java
@@ -7,13 +7,16 @@ package com.lwer0.modtools.inventories;
 
 import com.lwer0.modtools.ModTools;
 import static com.lwer0.modtools.inventories.BanInventory.baninv;
+import com.lwer0.modtools.inventories.permortemp.PermOrTempBan;
 import com.lwer0.modtools.utils.ColorUtil;
 import static com.lwer0.modtools.utils.ColorUtil.color;
 import java.util.ArrayList;
 import java.util.List;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.SkullType;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -37,10 +40,25 @@ public class MainInventory implements Listener {
     
     public static Inventory maininv = Bukkit.createInventory(null, 27, ColorUtil.color("&2ModTools"));    
     static {
-        ItemStack head = new ItemStack(Material.SKULL_ITEM, 1, (short)SkullType.PLAYER.ordinal());
-        SkullMeta sm = (SkullMeta)head.getItemMeta();
-        sm.setOwner(MainInventory.name);
-        head.setItemMeta(sm);
+        for(Player players : Bukkit.getOnlinePlayers()) {
+            if(!players.getName().equals(name)) {
+                
+                    List<String> lorename = new ArrayList<String>();
+                    lorename.add(color("&cJugador el cual"));
+                    lorename.add(color("&cva a ser baneado"));
+ 
+                    String pl = players.getName();
+                    ItemStack skull = new ItemStack(Material.SKULL_ITEM);
+                    skull.setDurability((short)3);
+                    SkullMeta sm = (SkullMeta) skull.getItemMeta();
+                    sm.setOwner(pl);
+                    sm.setDisplayName(ChatColor.AQUA + "" + pl);
+                    sm.setLore(lorename);
+                    skull.setItemMeta(sm);
+ 
+                    maininv.setItem(4, skull);
+            }
+         }
         //Item 1
         List<String> list = new ArrayList<String>();
         list.add(color("&7Desde aquí puedes"));
@@ -51,18 +69,25 @@ public class MainInventory implements Listener {
         im1.setLore(list);
         ns1.setItemMeta(im1);
         //Item 2
+        List<String> list2 = new ArrayList<String>();
+        list2.add(color("&7Expulsa al jugador"));
+        list2.add(color("&7del servidor"));
         ItemStack ns2 = new ItemStack (Material.NETHER_STAR, 1);
         ItemMeta im2 = ns1.getItemMeta();
+        im2.setLore(list2);
+        im2.setDisplayName(color("&cKick"));
+        ns2.setItemMeta(im2);
         //Item 3
         ItemStack ns3 = new ItemStack (Material.NETHER_STAR, 1);
         ItemMeta im3 = ns1.getItemMeta();
+        im3.setDisplayName(color("&bMute"));
+        ns3.setItemMeta(im3);
         //Item 4
         ItemStack ns4 = new ItemStack (Material.NETHER_STAR, 1);
         ItemMeta im4 = ns1.getItemMeta();
         //Item 5
         ItemStack ns5 = new ItemStack (Material.NETHER_STAR, 1);
         ItemMeta im5 = ns1.getItemMeta();
-        maininv.setItem(4, head);
         maininv.setItem(11, ns1);
         maininv.setItem(12, ns2);
         maininv.setItem(13, ns3);
@@ -75,7 +100,12 @@ public class MainInventory implements Listener {
         Player p = (Player)event.getWhoClicked();
         if (event.getClickedInventory().equals(maininv)) {
             if (event.getSlot() == 11) {
-                p.openInventory(baninv);
+                p.openInventory(PermOrTempBan.banpermortempinv);
+            } else if (event.getSlot() == 12) {
+                Player p2 = Bukkit.getServer().getPlayer(name);
+                p2.kickPlayer(color("&cHas sido expulsado por no respetar las normas de HideOutMC"));
+            } else if (event.getSlot() == 13) {
+                
             }
         }
     }
