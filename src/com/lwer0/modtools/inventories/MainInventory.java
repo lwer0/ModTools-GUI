d08bfd142cd5024479b2c477ebf81f81887fb3ad
Joel <Joel@192.168.1.177>
2016-06-18 17:11:41 +0200

    some things

--- src/com/lwer0/modtools/inventories/MainInventory.java
+++ src/com/lwer0/modtools/inventories/MainInventory.java
@@ -10,6 +10,7 @@ import com.lwer0.modtools.utils.ColorUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -18,16 +19,19 @@ public class MainInventory implements Listener {
     
     private static String name;
     
+    public void setName(String pname){
+        MainInventory.name = pname;
+    }
+    
     public static Inventory maininv = Bukkit.createInventory(null, 27, ColorUtil.color("&bModTools"));    
     static {
         ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
         ItemMeta meta = skull.getItemMeta();
         meta.setDisplayName(name);
         skull.setItemMeta(meta);
-
         maininv.setItem(4, skull);
     }
-
+    
     public MainInventory(ModTools aThis) {
     }
 
@@ -35,7 +39,4 @@ public class MainInventory implements Listener {
     }
 
     
-    public void setName(String name){
-        this.name = name;
-    }
 }
