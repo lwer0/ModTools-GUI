1867776c9b79072ea3b383427fc03d6280dbf7b7
Joel <Joel@192.168.1.177>
2016-06-18 18:17:42 +0200

    items

--- src/com/lwer0/modtools/inventories/MainInventory.java
+++ src/com/lwer0/modtools/inventories/MainInventory.java
@@ -7,10 +7,17 @@ package com.lwer0.modtools.inventories;
 
 import com.lwer0.modtools.ModTools;
 import com.lwer0.modtools.utils.ColorUtil;
+import static com.lwer0.modtools.utils.ColorUtil.color;
+import java.util.ArrayList;
+import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.SkullType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -29,11 +36,37 @@ public class MainInventory implements Listener {
     
     public static Inventory maininv = Bukkit.createInventory(null, 27, ColorUtil.color("&bModTools"));    
     static {
-        ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1 , (short) 3);
-        SkullMeta smeta = (SkullMeta) skull.getItemMeta();
-        smeta.setOwner(name);
-        skull.setItemMeta(smeta);
-        maininv.setItem(4, skull);
+        ItemStack head = new ItemStack(Material.SKULL_ITEM, 1, (short)SkullType.PLAYER.ordinal());
+        SkullMeta sm = (SkullMeta)head.getItemMeta();
+        sm.setOwner(MainInventory.name);
+        head.setItemMeta(sm);
+        //Item 1
+        List<String> list = new ArrayList<String>();
+        list.add(color("&eTest"));
+        ItemStack ns1 = new ItemStack (Material.NETHER_STAR, 1);
+        ItemMeta im1 = ns1.getItemMeta();
+        im1.setDisplayName(color("&4Ban"));
+        im1.setLore(list);
+        ns1.setItemMeta(im1);
+        //Item 2
+        ItemStack ns2 = new ItemStack (Material.NETHER_STAR, 1);
+        ItemMeta im2 = ns1.getItemMeta();
+        //Item 3
+        ItemStack ns3 = new ItemStack (Material.NETHER_STAR, 1);
+        ItemMeta im3 = ns1.getItemMeta();
+        //Item 4
+        ItemStack ns4 = new ItemStack (Material.NETHER_STAR, 1);
+        ItemMeta im4 = ns1.getItemMeta();
+        //Item 5
+        ItemStack ns5 = new ItemStack (Material.NETHER_STAR, 1);
+        ItemMeta im5 = ns1.getItemMeta();
+        maininv.setItem(4, head);
+        maininv.setItem(11, ns1);
+        maininv.setItem(12, ns2);
+        maininv.setItem(13, ns3);
+        maininv.setItem(14, ns4);
+        maininv.setItem(15, ns5);
+        
     }
     
     public MainInventory(ModTools aThis) {
