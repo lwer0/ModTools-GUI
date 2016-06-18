cf0ded7f9f0c9077f7dbaac351cceee6e499e22d
Joel <Joel@192.168.1.177>
2016-06-18 22:33:41 +0200

    new inventory move cancel

--- src/com/lwer0/modtools/listeners/InventoryMove.java
+++ src/com/lwer0/modtools/listeners/InventoryMove.java
@@ -8,6 +8,7 @@ package com.lwer0.modtools.listeners;
 import com.lwer0.modtools.ModTools;
 import com.lwer0.modtools.inventories.BanInventory;
 import com.lwer0.modtools.inventories.MainInventory;
+import com.lwer0.modtools.inventories.permortemp.PermOrTempBan;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -22,7 +23,7 @@ public class InventoryMove implements Listener {
     @EventHandler
     public void onInventoryClick (InventoryClickEvent event) {
         Player p = (Player)event.getWhoClicked();
-        if (event.getInventory().equals(MainInventory.maininv) || event.getInventory().equals(BanInventory.baninv)) {
+        if (event.getInventory().equals(MainInventory.maininv) || event.getInventory().equals(BanInventory.baninv) || event.getInventory().equals(PermOrTempBan.banpermortempinv)) {
             event.setCancelled(true);
             p.updateInventory();
         }
