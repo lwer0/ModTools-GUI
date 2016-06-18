51ea7c5da2febb2476f5e8f1dd537140945530a3
Joel <Joel@192.168.1.177>
2016-06-18 02:12:39 +0200

    creado de 0

---
+++ src/com/lwer0/modtools/commands/mtopen.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lwer0.modtools.commands;
+
+import com.lwer0.modtools.inventories.MainInventory;
+import static com.lwer0.modtools.inventories.MainInventory.maininv;
+import static com.lwer0.modtools.utils.ColorUtil.color;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+/**
+ *
+ * @author Joel
+ */
+public class mtopen implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player p = Bukkit.getServer().getPlayer(args[0]);
+        String pname = p.getName();
+        Player psender = (Player) sender;
+        MainInventory mainInventory = new MainInventory();
+        mainInventory.setName(args[0]);
+        if (cmd.getName().equalsIgnoreCase("mt")) {
+            if (sender.hasPermission("modtools.moderate")) {
+                if (args[0].length() == 0) {
+                    sender.sendMessage(color("&cArgumento invalido"));
+                    sender.sendMessage(color("&aCorrecto --> /mt <nick>"));
+                } else {
+                    psender.openInventory(maininv);
+                }
+            } else {
+                sender.sendMessage(color("&cNo Permissions"));
+            }
+        }
+        return false;
+    }
+    
+}
