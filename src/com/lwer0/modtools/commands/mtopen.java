a4cd8aa2041e9d092ebbbf5c3df0668314327e04
Joel <Joel@192.168.1.177>
2016-06-19 16:22:04 +0200

    Try

--- src/com/lwer0/modtools/commands/mtopen.java
+++ src/com/lwer0/modtools/commands/mtopen.java
@@ -8,7 +8,9 @@ package com.lwer0.modtools.commands;
 import com.lwer0.modtools.inventories.MainInventory;
 import static com.lwer0.modtools.inventories.MainInventory.maininv;
 import com.lwer0.modtools.inventories.permortemp.PermOrTempBan;
+import com.lwer0.modtools.inventories.permortemp.PermOrTempMute;
 import com.lwer0.modtools.inventories.temps.TempBansInv;
+import com.lwer0.modtools.inventories.temps.TempMuteInv;
 import static com.lwer0.modtools.utils.ColorUtil.color;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -24,15 +26,23 @@ public class mtopen implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player psender = (Player) sender;
-        MainInventory mainInventory = new MainInventory();
-        mainInventory.setName(args[0]);
-        PermOrTempBan permortempban = new PermOrTempBan();
-        permortempban.setName(args[0]);
-        TempBansInv tempbansinv = new TempBansInv();
-        tempbansinv.setName(args[0]);
+        try {
+            MainInventory mainInventory = new MainInventory();
+            mainInventory.setName(args[0]);
+            PermOrTempBan permortempban = new PermOrTempBan();
+            permortempban.setName(args[0]);
+            TempBansInv tempbansinv = new TempBansInv();
+            tempbansinv.setName(args[0]);
+            PermOrTempMute permortempmute = new PermOrTempMute();
+            permortempmute.setName(args[0]);
+            TempMuteInv tempmuteinv = new TempMuteInv();
+            tempmuteinv.setName(args[0]);
+        } catch (Exception e) {
+            psender.sendMessage(color("&cError al establecer un usuario como nominado a la sancion"));
+        }
         if (cmd.getName().equalsIgnoreCase("mt")) {
             if (sender.hasPermission("modtools.moderate")) {
-                if (args[0].length() == 0) {
+                if (args.length == 0) {
                     sender.sendMessage(color("&cArgumento invalido"));
                     sender.sendMessage(color("&aCorrecto --> /mt <nick>"));
                 } else {
